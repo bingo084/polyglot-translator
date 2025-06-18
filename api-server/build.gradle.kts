6bef13845b48b96db21b14d3e77d394086b22f19
王斌 <bingo084@outlook.com>
2025-06-18 11:22:56 +0800

    chore(api-server): remove unused dependency

--- api-server/build.gradle.kts
+++ api-server/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins { id("com.google.devtools.ksp") version "2.1.21+" }
 
 val jimmerVersion = "0.9.93"
-val idgenerator = "1.0.6"
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-web")
@@ -15,8 +14,6 @@ dependencies {
   ksp("org.babyfish.jimmer:jimmer-ksp:$jimmerVersion")
   // Database
   runtimeOnly("org.postgresql:postgresql")
-  // IdGenerator
-  implementation("com.github.yitter:yitter-idgenerator:${idgenerator}")
   // Kafka
   implementation("org.springframework.kafka:spring-kafka")
 }
